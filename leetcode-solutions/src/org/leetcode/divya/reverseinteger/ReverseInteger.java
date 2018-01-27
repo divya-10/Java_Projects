e2779e5be84779c492f4dc25a2772f5b32e020da
divya vippala <shilpareddy.vippala@gmail.com>
2018-01-27 14:24:23 -0600

    intial commit to reverse integer problem

---
+++ leetcode-solutions/src/org/leetcode/divya/reverseinteger/ReverseInteger.java
@@ -0,0 +1,29 @@
+package org.leetcode.divya.reverseinteger;
+
+/**
+ * <p>
+ * Given a 32-bit signed integer, reverse digits of an integer.
+ * </p>
+ * <p>
+ * Example 1:
+ * 
+ * Input: 123 Output: 321 Example 2:
+ * 
+ * Input: -123 Output: -321 Example 3:
+ * </p>
+ * Input: 120 Output: 21 Note: Assume we are dealing with an environment which
+ * could only hold integers within the 32-bit signed integer range. For the
+ * purpose of this problem, assume that your function returns 0 when the
+ * reversed integer overflows.
+ * 
+ * @author Praveen
+ *
+ */
+public class ReverseInteger {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
